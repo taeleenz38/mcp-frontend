382e5e98f26b84d368b2219ff813f1eb1d8ff7a1
Tae Kyu Lee <taekyu.lee3@gmail.com>
2025-07-11 14:05:54 +1000

    chat input WIP

--- app/components/Chat.tsx
+++ app/components/Chat.tsx
@@ -1,11 +1,24 @@
 import React from "react";
+import Image from "next/image";
 
 const Chat = () => {
   return (
-    <div className="w-full fixed bottom-0 flex justify-center">
-      <div className="flex flex-col w-3/4 p-4 border-2 rounded-lg">
-        <input className=""></input>
-        <button>click</button>
+    <div className="w-full fixed bottom-14 flex justify-center">
+      <div className="flex flex-col w-1/2 p-6 border-2 border-gray-500 rounded-lg">
+        <input className="mb-3 outline-none focus:outline-none focus:ring-0 focus:border-transparent" placeholder="Ask anything">
+        </input>
+        <div className="flex justify-between">
+          <div></div>
+          <button className="rounded-full">
+            <Image
+              src="/enter.png"
+              width={28}
+              height={28}
+              alt="chat-input-button"
+              className="rounded-full"
+            />
+          </button>
+        </div>
       </div>
     </div>
   );
