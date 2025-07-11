4f256ba4d0f493e68ff1a275e9df4df65835b8b2
Tae Kyu Lee <taekyu.lee3@gmail.com>
2025-07-11 13:46:30 +1000

    chat window WIP

---
+++ app/components/Chat.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Chat = () => {
+  return (
+    <div className="w-full fixed bottom-0 flex justify-center">
+      <div className="flex flex-col w-3/4 p-4 border-2 rounded-lg">
+        <input className=""></input>
+        <button>click</button>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import Chat from "./components/Chat";
 
 export default function Home() {
-  return <div className="w-screen h-screen"></div>;
+  return (
+    <div className="w-screen h-screen">
+      <Chat />
+    </div>
+  );
 }
