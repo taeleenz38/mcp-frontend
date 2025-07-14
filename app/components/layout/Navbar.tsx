531185e43fb4b4ff7ad67ab0f8d44c022e2393cd
Tae Kyu Lee <taekyu.lee3@gmail.com>
2025-07-14 12:56:34 +1000

    sidebar gap

--- app/components/layout/Navbar.tsx
+++ app/components/layout/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className="fixed left-0 top-0 h-screen w-1/5 bg-black text-white flex flex-col items-center justify-start pt-11">
       <Image src="/logo.png" alt="logo" width={125} height={125} className="" />
-      <div className="flex flex-col mt-10 gap-2">
+      <div className="flex flex-col mt-10 gap-3">
         <div className="flex justify-start gap-1 hover:scale-105 duration-200">
           <Image src="/new-chat.png" width={28} height={28} alt="new-chat" />
           <Link className="text-lg" href="/">
